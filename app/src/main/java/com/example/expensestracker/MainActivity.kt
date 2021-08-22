60a26ab2842cff81fd36b9de6aeb36e881433658
mrtsv <a.martsev85@gmail.com>
2021-08-22 03:59:36 +0300

    creating fragments and layouts.v2

--- app/src/main/java/com/example/expensestracker/MainActivity.kt
+++ app/src/main/java/com/example/expensestracker/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.expensestracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.expensestracker.databinding.ActivityMainBinding
+import com.example.expensestracker.login.LoginFragment
+import com.example.expensestracker.main.MainFragment
 import org.koin.android.ext.android.bind
 
 private lateinit var binding: ActivityMainBinding
@@ -14,6 +16,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 //        binding.bottomNavView.background = null
 //        binding.bottomNavView.menu.getItem(2).isEnabled = false
+        showFragment()
+    }
 
+    private fun showFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(binding.navHostFragment.id, LoginFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun replaceFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.navHostFragment.id, MainFragment())
+            .addToBackStack(null)
+            .commit()
     }
 }
--- app/src/main/java/com/example/expensestracker/login/LoginFragment.kt
+++ app/src/main/java/com/example/expensestracker/login/LoginFragment.kt
@@ -4,25 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.expensestracker.MainActivity
+import com.example.expensestracker.databinding.FragmentLoginBinding
+import org.koin.android.ext.android.bind
 
 class LoginFragment() : Fragment() {
 
-    private var binding: LoginFragmentBinding? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var binding: FragmentLoginBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val buttonConfirm: Button? = binding?.buttonConfirm
+        buttonConfirm?.setOnClickListener{clickListener(buttonConfirm)}
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    private fun clickListener(view: View) {
+        (activity as? MainActivity)?.replaceFragment()
     }
 }
