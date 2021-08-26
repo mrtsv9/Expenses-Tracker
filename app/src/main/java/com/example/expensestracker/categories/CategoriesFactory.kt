3ca4258237470d3d5175881601cd3249bcffb1dd
mrtsv <a.martsev85@gmail.com>
2021-08-26 20:37:13 +0300

    categories + accounts

--- app/src/main/java/com/example/expensestracker/categories/CategoriesFactory.kt
+++ app/src/main/java/com/example/expensestracker/categories/CategoriesFactory.kt
@@ -9,14 +9,14 @@ class CategoriesFactory {
         list.add(SingleCategory("Cafe", R.drawable.ic_coffee_cup, 300))
         list.add(SingleCategory("Movies", R.drawable.ic_movie, 300))
         list.add(SingleCategory("Transport", R.drawable.ic_public_transport, 300))
-        list.add(SingleCategory("Health", R.drawable.ic_shopping_basket, 300))
-        list.add(SingleCategory("Gifts", R.drawable.ic_shopping_basket, 300))
-        list.add(SingleCategory("Family", R.drawable.ic_shopping_basket, 300))
-        list.add(SingleCategory("Shopping", R.drawable.ic_shopping_basket, 300))
-        list.add(SingleCategory("Sport", R.drawable.ic_shopping_basket, 300))
-        list.add(SingleCategory("Home things", R.drawable.ic_shopping_basket, 300))
-        list.add(SingleCategory("Investment", R.drawable.ic_shopping_basket, 300))
-        list.add(SingleCategory("Internet", R.drawable.ic_shopping_basket, 300))
+        list.add(SingleCategory("Health", R.drawable.ic_healthcare, 300))
+        list.add(SingleCategory("Gifts", R.drawable.ic_gift_box, 300))
+        list.add(SingleCategory("Family", R.drawable.family, 300))
+        list.add(SingleCategory("Shopping", R.drawable.ic_online_shopping, 300))
+        list.add(SingleCategory("Sport", R.drawable.ic_dumbbell, 300))
+        list.add(SingleCategory("Home things", R.drawable.ic_home, 300))
+        list.add(SingleCategory("Investment", R.drawable.ic_investment, 300))
+        list.add(SingleCategory("Internet", R.drawable.ic_world_grid, 300))
         return list
     }
 
